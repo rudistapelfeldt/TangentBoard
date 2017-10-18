778ba6a55758d384748b8e62ad138e45f98c1d32
rudistapelfeldt <rudi.stapelfeldt@lavalamp.biz>
2017-10-18 15:36:21 +0200

    Request URLs added

---
+++ app/src/main/java/com/tangent/assessment/tangentboard/apiservice/Request.java
@@ -0,0 +1,28 @@
+package com.tangent.assessment.tangentboard.apiservice;
+
+/**
+ * Created by Rudi Stapelfeldt on 2017/10/18.
+ * Name: Request.java
+ * Description: Contains constants used to access Tangent api
+ */
+
+public class Request {
+
+    //BASE URL FOR REQUESTS TO CONNECT TO
+    public static final String BASE_URL_API = "http://staging.tangent.tngnt.co";
+
+    //AUTH TOKEN OR LOGIN
+    public static final String AUTH_TOKEN = "/api-token-auth/";
+
+    //USER DETAILS
+    public static final String USER_DETAILS = "/api/user/me/";
+
+    //EMPLOYEE DETAIL
+    public static final String EMPLOYEE_DETAIL = "/api/employee/";
+
+    //USER EMPLOYEE PROFILE
+    public static final String USER_EMPLOYEE_PROFILE = "/api/employee/me/";
+
+
+
+}
