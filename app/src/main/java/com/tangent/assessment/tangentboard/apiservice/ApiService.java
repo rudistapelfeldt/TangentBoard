a857174525640ea5c3f01e24a84b8c385ce0ac81
rudistapelfeldt <rudi.stapelfeldt@lavalamp.biz>
2017-10-18 20:26:30 +0200

    fixed user detail call

--- app/src/main/java/com/tangent/assessment/tangentboard/apiservice/ApiService.java
+++ app/src/main/java/com/tangent/assessment/tangentboard/apiservice/ApiService.java
@@ -5,6 +5,7 @@ import com.tangent.assessment.tangentboard.model.UserData;
 
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import rx.Observable;
@@ -21,6 +22,6 @@ public interface ApiService {
     Observable<LoginData> getAuthToken(@Field("username") String username, @Field("password") String password);
 
     @Headers({"Content-Type: application/json"})
-    @POST(Request.USER_DETAILS)
+    @GET(Request.USER_DETAILS)
     Observable<UserData> getMyDetails();
 }
